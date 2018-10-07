ee48703aca161d94693718ee1d6e2717b880fbad
amzi <amitzilber@gmail.com>
2018-10-07 21:05:06 +0300

    third commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
   <Layout>
     <h1>Hey you</h1>
     <p>My name is Amit Zilberstein and I'm learning web development with FCC!</p>
-    <p>Until recently I have managed the Hebrew University Entrepreneruship Center, look at<a href='www.hustart.com'>www.HUstart.com</a></a></p>
+    <p>Until recently I have managed the Hebrew University Entrepreneruship Center, look at<a href='www.hustart.com'>www.HUstart.com</a></p>
     <p>Welcome to my first Gatsby site.</p>
     <p>Now I'm going to build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
